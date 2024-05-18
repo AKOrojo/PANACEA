43894ae171343de08341cbd7461db0d9c7138826
abanisenioluwaorojo <abanisenioluwa_oroj1@baylor.edu>
2024-05-17 19:09:50 -0500

    policy mongo

--- src/access_control_view/main_pipeline.py
+++ src/access_control_view/main_pipeline.py
@@ -7,21 +7,28 @@ combined_pipeline = [
                 function getRandomPoliciesAndMeta() {
                     var purposes = ["research", "administration", "marketing", "sales", "development", "testing", "customer support"];
                     var policies = [];
-                    var meta = {};
+                    var meta = {"aip": [], "pip": []};
                     var has_policy = Math.random() < 0.5;  // 50% probability to have at least one policy
                     if (has_policy) {
                         var num_policies = Math.floor(Math.random() * 3) + 1;  // From 1 to 3 policies
                         for (var i = 0; i < num_policies; i++) {
                             var is_positive = Math.random() < 0.5;
                             var purpose = purposes[Math.floor(Math.random() * purposes.length)];
-                            if (!meta[purpose]) {
-                                meta[purpose] = ["allowed"];
+                            if (is_positive) {
+                                meta.aip.push(purpose);
+                                var policy = {
+                                    exp: "s.ap in meta.aip && s.ap == '" + purpose + "'",
+                                    tp: "positive"
+                                };
+                                policies.push(policy);
+                            } else {
+                                meta.pip.push(purpose);
+                                var policy = {
+                                    exp: "s.ap in meta.pip && s.ap == '" + purpose + "'",
+                                    tp: "negative"
+                                };
+                                policies.push(policy);
                             }
-                            var policy = {
-                                exp: "s.ap in meta." + purpose,
-                                tp: is_positive ? "positive" : "negative"
-                            };
-                            policies.push(policy);
                         }
                     }
                     return {policies: policies, meta: meta};
